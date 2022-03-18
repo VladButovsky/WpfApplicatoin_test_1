52adb6b9f1d67cfee37db8241f4cf13494089a8f
VladButovsky <83472569+VladButovsky@users.noreply.github.com>
2022-03-18 13:57:20 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplicatoin_test_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Registration_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = textBoxPassword.Password.Trim();
+            string email = textBoxEmail.Text.ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Login is too short";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (password.Length < 5)
+            {
+                textBoxPassword.ToolTip = "Password is too short";
+                textBoxPassword.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Password is too short";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            { 
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                textBoxPassword.ToolTip = "";
+                textBoxPassword.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Ok!");
+
+            }
+        }
+    }
+}
